57bd6721b86838c44cadf428e8d6d879698d4edc
Paulo Pinto <gomescasador@gmail.com>
2023-03-08 01:09:33 +0000

    Melhorado o layout do contas com distribuidor

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+function somarInputs(){
+    var inputValor = document.getElementsByTagName('input')
+    var resSoma = document.getElementById('#res-conta')
+    var teste = inputValor[0].value
+
+/*         for (passo = 0; passo <= inputValor.length; passo++) {
+            var Soma = Soma + inputValor[passo].value
+        } */
+    
+    console.log(inputValor)
+  
+}
+
+    
