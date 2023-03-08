6b8b0daa1f8270dcdee3f52408820f86bab5142a
Paulo Pinto <gomescasador@gmail.com>
2023-03-08 15:54:43 +0000

    Add mais um campo, neon color resposta

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,9 @@ function somarInputs(){
     var inputValor = document.getElementsByTagName('input')
     var resSoma = document.getElementById('resConta')
     var resMsg = document.getElementById('resMsg')
-    var soma = Number(inputValor[0].value)
+    var soma = Number(inputValor[0].value) + Number(inputValor[1].value)
 
-    for (passo = 1; passo < inputValor.length; passo++) {
+    for (passo = 2; passo < inputValor.length; passo++) {
 
         soma = soma - Number(inputValor[passo].value)
 
@@ -14,17 +14,22 @@ function somarInputs(){
         resMsg.innerHTML = 'Você precisa:'
         resSoma.innerHTML = `RECEBER: € ${Math.abs(soma).toFixed(2)}`
         resSoma.style.color = 'red'
-        resSoma
+        resSoma.classList.add('red')
+        resSoma.classList.remove('yellow', 'green')
     }
     else if (soma < 0) {
         resMsg.innerHTML = 'Você precisa:'
         resSoma.innerHTML = `ENTREGAR: € ${Math.abs(soma).toFixed(2)}`
         resSoma.style.color = '#B8860B'
+        resSoma.classList.add('yellow')
+        resSoma.classList.remove('red', 'green')
     }
     else {
         resMsg.innerHTML = 'Esta tudo certo'
         resSoma.innerHTML = `RESULTADO: € ${Math.abs(soma).toFixed(2)}`
         resSoma.style.color = 'green'
+        resSoma.classList.add('green')
+        resSoma.classList.remove('yellow', 'red')
     }
     
   
