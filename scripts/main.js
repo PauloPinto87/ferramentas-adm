31271e9ee93666df0f232dfb95d0e3bffb256315
Paulo Pinto <gomescasador@gmail.com>
2023-03-08 14:56:43 +0000

    Javascript da parte contas distri. avançado

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,33 @@
 function somarInputs(){
     var inputValor = document.getElementsByTagName('input')
-    var resSoma = document.getElementById('#res-conta')
-    var teste = inputValor[0].value
+    var resSoma = document.getElementById('resConta')
+    var resMsg = document.getElementById('resMsg')
+    var soma = Number(inputValor[0].value)
 
-/*         for (passo = 0; passo <= inputValor.length; passo++) {
-            var Soma = Soma + inputValor[passo].value
-        } */
+    for (passo = 1; passo < inputValor.length; passo++) {
+
+        soma = soma - Number(inputValor[passo].value)
+
+    }
+
+    if (soma > 0) {
+        resMsg.innerHTML = 'Você precisa:'
+        resSoma.innerHTML = `RECEBER: € ${Math.abs(soma).toFixed(2)}`
+        resSoma.style.color = 'red'
+        resSoma
+    }
+    else if (soma < 0) {
+        resMsg.innerHTML = 'Você precisa:'
+        resSoma.innerHTML = `ENTREGAR: € ${Math.abs(soma).toFixed(2)}`
+        resSoma.style.color = '#B8860B'
+    }
+    else {
+        resMsg.innerHTML = 'Esta tudo certo'
+        resSoma.innerHTML = `RESULTADO: € ${Math.abs(soma).toFixed(2)}`
+        resSoma.style.color = 'green'
+    }
     
-    console.log(inputValor)
+  
   
 }
 
